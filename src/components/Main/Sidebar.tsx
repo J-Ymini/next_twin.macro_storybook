e1035516fd5e060893bb131eec7ce694577d2582
yongmin.jeon <dydalsdl1414@gmail.com>
2022-08-10 11:21:12 +0900

    feat: Add Sidebar

--- src/components/Main/Sidebar.tsx
+++ src/components/Main/Sidebar.tsx
@@ -1,7 +1,97 @@
-import React from "react";
+import { SVGProps } from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from "@heroicons/react/outline";
 
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+const Sidebar = (): JSX.Element => {
+  return (
+    <StyledWrapper>
+      <StyledMainIconImage src="https://static.vecteezy.com/system/resources/previews/002/534/045/original/social-media-twitter-logo-blue-isolated-free-vector.jpg" />
+      <ul>
+        {iconsList.map(({ id, text, Icon }) => (
+          <StyledIconsItem key={id} className="group">
+            <Icon />
+            <StyledIconsItemDescription>{text}</StyledIconsItemDescription>
+          </StyledIconsItem>
+        ))}
+      </ul>
+    </StyledWrapper>
+  );
 };
 
 export default Sidebar;
+
+const StyledWrapper = styled.div`
+  ${tw`flex flex-col`}
+`;
+
+const StyledMainIconImage = styled.img`
+  ${tw`w-10 h-10`}
+`;
+
+const StyledIconsItem = styled.li`
+  ${tw`flex cursor-pointer items-center max-w-min space-x-2 px-4 py-3 rounded-full 
+  transition-all duration-200 hover:(bg-gray-100)`}
+
+  svg {
+    ${tw`h-6 w-6`}
+  }
+`;
+
+const StyledIconsItemDescription = styled.p`
+  ${StyledIconsItem}:hover & {
+    ${tw`text-twitter`}
+  }
+`;
+
+const iconsList = [
+  {
+    id: 1,
+    text: "home",
+    Icon: HomeIcon,
+  },
+  {
+    id: 2,
+    text: "Explore",
+    Icon: HashtagIcon,
+  },
+  {
+    id: 3,
+    text: "Notifications",
+    Icon: BellIcon,
+  },
+  {
+    id: 4,
+    text: "Messages",
+    Icon: MailIcon,
+  },
+  {
+    id: 5,
+    text: "Bookmarks",
+    Icon: BookmarkIcon,
+  },
+  {
+    id: 6,
+    text: "Lists",
+    Icon: CollectionIcon,
+  },
+  {
+    id: 7,
+    text: "More",
+    Icon: DotsCircleHorizontalIcon,
+  },
+  {
+    id: 8,
+    text: "SignIn",
+    Icon: UserIcon,
+  },
+];
